14da1cb8df71d3b4613ecac6ae5eae93d6579771
Achthar <69119535+Achthar@users.noreply.github.com>
2025-08-24 23:28:26 +0200

    add data-sdk

--- packages/calldata-sdk/src/evm/lending/utils/index.ts
+++ packages/calldata-sdk/src/evm/lending/utils/index.ts
@@ -1,16 +1,12 @@
 import {
-  COMETS_PER_CHAIN_MAP,
-  COMPOUND_BASE_TOKENS,
-  COMPOUND_V2_STYLE_TOKENS,
   Lender,
-  MORPHO_BLUE_POOL_DATA,
-  AAVE_FORK_POOL_DATA,
-  AAVE_STYLE_TOKENS,
   AAVE_V2_LENDERS,
   AAVE_LENDERS,
   WRAPPED_NATIVE_INFO,
+  COMPOUND_V3_LENDERS,
+  COMPOUND_V2_LENDERS,
 } from '@1delta/asset-registry'
-import { COMPOUND_V2_LENDERS, COMPOUND_V3_LENDERS, LenderData, LenderGroups } from '../types'
+import { LenderData, LenderGroups } from '../types'
 import { ComposerCommands, FlashLoanIds, LenderIds, PermitIds, uint16 } from '@1delta/calldatalib'
 import { Address, encodePacked, Hex, zeroAddress } from 'viem'
 import { ChainIdLike, SerializedCurrencyAmount } from '@1delta/type-sdk'
@@ -18,20 +14,20 @@ import { FLASH_LOAN_PROVIDERS, FlashLoanProvider } from '../../../utils'
 import { BALANCER_V2_FORKS } from '@1delta/dex-registry'
 import { FlashInfo } from '../../flashloan/types/marginHandlers'
 import { isAaveV2Type, isAaveV3Type, isCompoundV2, isCompoundV3 } from '../../flashloan'
+import { aavePools, aaveTokens, compoundV2Tokens, compoundV3BaseData, compoundV3Pools, morphoPools } from '@1delta/data-sdk'
 
 export * from './permit'
 
 export function getLenderData(lender: Lender, chainId: ChainIdLike, asset: string): LenderData {
   // check if the lender is aave (all aave forks)
   if (AAVE_LENDERS.includes(lender)) {
-    const key = lender as keyof typeof AAVE_FORK_POOL_DATA
-    // @ts-ignore
-    const { pool } = AAVE_FORK_POOL_DATA?.[key]?.[chainId]
-    const aaveTokens = AAVE_STYLE_TOKENS[key]
-    const chainKey = chainId as keyof typeof aaveTokens
+    const key = lender
+    const { pool } = aavePools()?.[key]?.[chainId]
+    const aaveTokensForFork = aaveTokens()[key]
+    const chainKey = chainId
     // aave only accepts wNative, adjust for this here
     const _asset = asset === zeroAddress ? WRAPPED_NATIVE_INFO[chainId].address : asset
-    const { aToken, sToken, vToken } = aaveTokens[chainKey][_asset.toLowerCase()] // asset addresses in asset-registry are not checksum addresses
+    const { aToken, sToken, vToken } = aaveTokensForFork[chainKey][_asset.toLowerCase()] // asset addresses in asset-registry are not checksum addresses
     const group = AAVE_V2_LENDERS.includes(lender) ? LenderGroups.AaveV2 : LenderGroups.AaveV3
     return {
       lender,
@@ -48,10 +44,10 @@ export function getLenderData(lender: Lender, chainId: ChainIdLike, asset: strin
 
   // check if the lender is compoundV2
   if (COMPOUND_V2_LENDERS.includes(lender)) {
-    const key = lender as keyof typeof COMPOUND_V2_STYLE_TOKENS
+    const key = lender
     // Convert asset address to lowercase to match the registry format
     const normalizedAsset = asset.toLowerCase()
-    const cToken = COMPOUND_V2_STYLE_TOKENS[key][chainId][normalizedAsset]
+    const cToken = compoundV2Tokens()[key]?.[chainId]?.[normalizedAsset]
     return {
       lender,
       pool: undefined,
@@ -67,8 +63,8 @@ export function getLenderData(lender: Lender, chainId: ChainIdLike, asset: strin
 
   // check if the lender is compoundV3
   if (COMPOUND_V3_LENDERS.includes(lender)) {
-    const comet = COMETS_PER_CHAIN_MAP[chainId][lender] as Address
-    const base = COMPOUND_BASE_TOKENS[lender][chainId].baseAsset as Address
+    const comet = compoundV3Pools()[chainId][lender] as Address
+    const base = compoundV3BaseData()[lender][chainId].baseAsset as Address
     return {
       lender,
       pool: comet,
@@ -84,10 +80,10 @@ export function getLenderData(lender: Lender, chainId: ChainIdLike, asset: strin
 
   // check if the lender is morpho
   if (lender === Lender.MORPHO_BLUE) {
-    if (Object.keys(MORPHO_BLUE_POOL_DATA).includes(lender.toString())) {
-      const lenderData = MORPHO_BLUE_POOL_DATA[lender as keyof typeof MORPHO_BLUE_POOL_DATA]
+    if (Object.keys(morphoPools()).includes(lender.toString())) {
+      const lenderData = morphoPools()[lender]
       if (Object.keys(lenderData).includes(chainId.toString())) {
-        const mb = lenderData[chainId as keyof typeof lenderData]
+        const mb = lenderData[chainId]
         return {
           lender,
           pool: mb as Address,
@@ -109,27 +105,26 @@ const BALANCER_FORKS = Object.keys(BALANCER_V2_FORKS)
 export function getFlashInfo(provider: FlashLoanProvider, chainId: ChainIdLike, composer: string): FlashInfo {
   // check if the lender is aave (all aave forks)
   if (AAVE_LENDERS.includes(provider as any)) {
-    // @ts-ignore
-    const pool = AAVE_FORK_POOL_DATA?.[provider]?.[chainId].pool
+    const pool = aavePools()?.[provider]?.[chainId].pool
     return {
       data: FLASH_LOAN_PROVIDERS[chainId][provider],
       // @ts-ignore
       poolType: AAVE_V2_LENDERS.includes(provider) ? FlashLoanIds.AAVE_V2 : FlashLoanIds.AAVE_V3,
       provider,
       providerAddress: pool,
-      balanceHolder: composer
+      balanceHolder: composer,
     }
   }
 
   if (provider === FlashLoanProvider.MORPHO) {
     // @ts-ignore
-    const pool = MORPHO_BLUE_POOL_DATA?.[provider]?.[chainId]
+    const pool = morphoPools?.[provider]?.[chainId]
     return {
       data: { id: 0, fee: 0n },
       poolType: FlashLoanIds.MORPHO,
       provider,
       providerAddress: pool,
-      balanceHolder: composer
+      balanceHolder: composer,
     }
   }
 
@@ -140,13 +135,12 @@ export function getFlashInfo(provider: FlashLoanProvider, chainId: ChainIdLike, 
       poolType: FlashLoanIds.BALANCER_V2,
       provider,
       providerAddress: pool,
-      balanceHolder: pool
+      balanceHolder: pool,
     }
   }
 
   throw new Error('Unsupported flash source')
 }
-
 
 export function getAssetParamsFromAmount(amount: SerializedCurrencyAmount) {
   return {
@@ -184,7 +178,7 @@ export function getLenderId(lender: LenderGroups) {
 export function encodePermit(asset: string, permitId: PermitIds, permitData: Hex): Hex {
   return encodePacked(
     ['uint8', 'uint8', 'address', 'uint16', 'bytes'],
-    [ComposerCommands.PERMIT, permitId, asset as Address, uint16(permitData.length / 2 - 1), permitData],
+    [ComposerCommands.PERMIT, permitId, asset as Address, uint16(permitData.length / 2 - 1), permitData]
   )
 }
 
