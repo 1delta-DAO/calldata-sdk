4378fafc9ee0fbce2ded9c7457d32de13dd5ee84
Max <0xmaxyz@gmail.com>
2025-09-17 11:12:26 +0330

    add zapIn to marginTrade type with its own props

--- packages/calldata-sdk/src/evm/flashloan/types/margin.ts
+++ packages/calldata-sdk/src/evm/flashloan/types/margin.ts
@@ -1,12 +1,14 @@
 import { Lender } from '@1delta/asset-registry'
 import { AaveInterestMode, MorphoParams } from '../../lending/types'
-import { PermitData } from '../..'
+import { GenericTrade, PermitData } from '../..'
+import { SerializedCurrencyAmount } from '@1delta/type-sdk'
 
 export enum MarginTradeType {
   Open = 'Open',
   Close = 'Close',
   CollateralSwap = 'CollateralSwap',
   DebtSwap = 'DebtSwap',
+  ZapIn = 'ZapIn',
 }
 
 export interface MarginData {
@@ -24,4 +26,17 @@ export interface MarginData {
   morphoParamsOut?: MorphoParams
   /** permit */
   permitData?: PermitData
+  /** zap data */
+  zapData?: ZapData
+}
+
+export interface ZapData {
+  inAsset: SerializedCurrencyAmount
+  /** if true, use the collateral asset, otherwise use the debt asset */
+  useCollateralAsset: boolean
+  trades: GenericTrade[]
+  /** Explicit borrow amount (debt asset) used to size the flash loan */
+  borrowAmount?: SerializedCurrencyAmount
+  /** Optional permit to pull user funds */
+  userPermit?: PermitData
 }
